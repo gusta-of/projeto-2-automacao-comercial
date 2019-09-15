ea734617ff4b943c20756b89b96571ba5774ea8e
Gustavo Fernandes <gustavoofernandes9712@gmail.com>
2019-09-15 17:17:59 -0300

    gustavo-fernandes: Cria modulo do component caixa

---
+++ CashierDocManager/src/app/caixa/caixa.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CaixaComponent } from './caixa.component';
+
+
+
+@NgModule({
+  declarations: [CaixaComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CaixaModule { }
