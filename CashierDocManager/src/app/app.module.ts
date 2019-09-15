312f837d1b7582c2a146402ae175446ed67eee76
Gustavo Fernandes <gustavoofernandes9712@gmail.com>
2019-09-15 18:53:06 -0300

    gustavo-fernandes: Adiciona menu lateral esquerdo e dependências

--- CashierDocManager/src/app/app.module.ts
+++ CashierDocManager/src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CaixaComponent } from './caixa/caixa.component';
+import { CaixaModule } from './caixa/caixa.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CaixaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ CashierDocManager/src/app/caixa/caixa-menu-esquerdo/caixa-menu-esquerdo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-caixa-menu-esquerdo',
+  templateUrl: './caixa-menu-esquerdo.component.html',
+  styleUrls: ['./caixa-menu-esquerdo.component.scss']
+})
+export class CaixaMenuEsquerdoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
