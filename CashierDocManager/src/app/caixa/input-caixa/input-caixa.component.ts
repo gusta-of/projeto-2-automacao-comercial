d3d5f509c2f76caa18bce4e7aa816bf3ccf891bd
Gustavo <gustavo.muller.sousa@gmail.com>
2019-09-19 01:10:28 -0300

    teste

--- CashierDocManager/src/app/caixa/input-caixa/input-caixa.component.ts
+++ CashierDocManager/src/app/caixa/input-caixa/input-caixa.component.ts
@@ -12,20 +12,29 @@ export class InputCaixaComponent implements OnInit {
 
   text: string;
 
-  pessoasResultado: string[];
-  pessoas: string[];
+  pessoasResultado: PessoaModel[];
+  pessoas: PessoaModel[] = [];
   
   ngOnInit() {
     this.MontePessoas();
   }
 
   MontePessoas() {
-    this.pessoas.push("João");
-    this.pessoas.push("Raimundis");
+    let pessoa1= new PessoaModel();
+    pessoa1.nome = "João";
+    pessoa1.cpf = "123456789";
+
+    let pessoa = new PessoaModel();
+    pessoa.nome = "Raimundis";
+    pessoa.cpf = "987654321";
+
+    this.pessoas.push(pessoa1);
+    this.pessoas.push(pessoa);
   }
 
   search(event): void {
-    this.pessoasResultado = this.pessoas.filter(c => c.startsWith(event.query));
+    this.pessoasResultado = [];
+    this.pessoasResultado = this.pessoas.filter(c => c.nome.startsWith(event.query));
   }
 
 
