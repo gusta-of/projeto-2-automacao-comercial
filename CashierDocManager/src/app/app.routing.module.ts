5b0b83af9a7f751661bec09c1455d6e718a7a3aa
Gustavo-Fernandes <32605131+gusta-of@users.noreply.github.com>
2019-09-27 22:10:58 -0300

    Aplica padrão lazy Loading (#16)

--- CashierDocManager/src/app/app.routing.module.ts
+++ CashierDocManager/src/app/app.routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes = [
     { path: '', component: HomePageComponent },
-    { path: 'home', component: HomePageComponent }
+    { path: 'home', component: HomePageComponent },
+    { path: 'caixa', loadChildren: ()=> import("./caixa/caixa.module").then(m => m.CaixaModule )}
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
+    imports: [RouterModule.forRoot(routes,  { preloadingStrategy: PreloadAllModules })],
     exports: [RouterModule]
 })
 export class AppRoutingModule {}
