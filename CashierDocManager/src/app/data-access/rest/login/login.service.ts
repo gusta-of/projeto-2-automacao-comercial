c20e56b9e3b1666e4ca9af71fd56fef927334efa
Gustavo Fernandes <gustavoofernandes9712@gmail.com>
2019-11-01 23:40:10 -0300

    Esconde menu na tela de login

--- CashierDocManager/src/app/data-access/rest/login/login.service.ts
+++ CashierDocManager/src/app/data-access/rest/login/login.service.ts
@@ -1,5 +1,5 @@
 
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -11,6 +11,7 @@ export class LoginService {
 
     public token: string;
     private url = 'https://localhost:44334/api/operador/autenticacao';
+    mostraMenuEmitter = new EventEmitter<boolean>();
 
     constructor(private _http: HttpClient, private router: Router) {
         const currentUser = JSON.parse(localStorage.getItem('currentUser'));
@@ -19,24 +20,31 @@ export class LoginService {
     
       /** Autentica Usuário */
       login(username: string, password: string): Observable<Operador> {
+        
         return this._http.post<Operador>(this.url, { username: username, password: password })
           .pipe(
             map(operador => {
               /** Retorna login bem-sucedido se houver um token jwt na resposta */
-              if (operador) {
+              if (operador && operador['token']) {
                 /**  armazenar detalhes do operador e token jwt no localStorage para 
                  * manter o operador logado entre as atualizações da página */
                 localStorage.setItem('currentUser', JSON.stringify(operador));
               }
+              if(localStorage.getItem('currentUser') != null){
+                this.mostraMenuEmitter.emit(true);  
+              }
               return operador;
             })
           );
+
+         
       }
     
       /** Remove autenticação do usuário do localStorage */
       logout(): void {
         /** Limpa o token removendo o operador do local store para efetuar o logout */
         this.token = null;
+        
         localStorage.removeItem('currentUser');
         this.router.navigate(["login"]);
       }
